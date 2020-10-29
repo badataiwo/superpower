f09d6c1d8bb7bf1fc8e5e3fd1250abf3114b9964
Lincoln S. Matra <lmatra@bentley.edu>
2020-10-29 10:18:53 -0400

    Initial commit

---
+++ app/controllers/HomeController.scala
@@ -0,0 +1,31 @@
+package controllers
+
+import javax.inject._
+import play.api._
+import play.api.mvc._
+import play.api.i18n._
+
+/**
+ * This controller creates an `Action` to handle HTTP requests to the
+ * application's home page.
+ */
+@Singleton
+class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController with I18nSupport {
+
+  //Setup an application logger
+  val appLogger: Logger = Logger("application")
+  
+  def index() = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index("Superheros"))
+  }
+  
+  
+  def loadPage(name: String) = Action { implicit request: Request[AnyContent] =>
+    appLogger.debug(s"Loading page: $name")
+    
+    name match {
+      //case "lincoln"  => Ok(views.html.lincoln(name.capitalize))
+      case _          => Ok(views.html.index("Superheros"))
+    }
+  }
+}
