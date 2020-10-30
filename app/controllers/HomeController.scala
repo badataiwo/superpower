a64543ada0e862359965f8cb6766417cb9857594
laresx <xu_lian@bentley.edu>
2020-10-30 11:14:36 -0400

    add new files for Lares Xu

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -28,6 +28,7 @@ class HomeController @Inject()(val controllerComponents: ControllerComponents) e
       case "taiwo"   => Ok(views.html.taiwo(name.capitalize))
       case "varsha"  => Ok(views.html.varsha(name.capitalize))
       case "ellis"   => Ok(views.html.ellis(name.capitalize))
+      case "lares"   => Ok(views.html.lares(name.capitalize))
       case _          => Ok(views.html.index("Superheros"))
     }
   }
